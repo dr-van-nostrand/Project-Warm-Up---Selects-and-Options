18681bdb3ae82e1c5c018e41289057f84866ae46
Lucas Zubeldia <lzubeldia@gmail.com>
2020-02-24 15:29:37 -0300

    VFinal

--- js/script.js
+++ js/script.js
@@ -16,9 +16,9 @@
 
 // 1) Follow the steps below to hide the "Select a shirt" option tag so users won't be able to select it or see it in the drop down menu
   // 1a) Target the "Select a shirt" `option` element with a querySelector, like so — ('option[value="Select a shirt"]') 
-  const selectS = document.querySelector('option[value="Select a shirt"]');
+  const selectShirt = document.querySelector('option[value="Select a shirt"]');
   // 1b) Use dot notation to set the `hidden` property to true on the "Select a shirt" `option` element 
-  selectS.hidden = true;
+  selectShirt.hidden = true;
 
 
 
@@ -30,7 +30,11 @@
   console.log(shirtSelectElement);
   console.log(shirtOptionElements);
   // 2b) Use the second variable and bracket notation to select one of the option elements and try logging it out to confirm its value 
+  console.log(shirtOptionElements[3]);
+
   // 2c) Use dot notation to set the `selected` property to true on the `option` element you selected above
+  shirtOptionElements[0].selected = true;
+
   // 2d) Refresh the browser to see the new value in the `select` element
   // 2e) Experiment with setting the `selected` property to true or false on different option elements to see the results
 
@@ -42,15 +46,33 @@ shirtSelectElement.addEventListener('change', (event) => {
 
 // 3) Follow the steps below to highlight the selected shirt 
   // 3a) Log out the value of the `select` element with `event.target` technique — console.log(event.target.value);
+  console.log(event.target.value);
+
   // 3b) Create a variable to store the shirt `img` elements, using a querySelectorAll, like so — ('.shirt-image') 
+const shirtImage = document.querySelectorAll('.shirt-image');
+
   // 3c) Create a `for` loop to iterate over the shirt `img` elements in the variable above
   // 3d) Inside the loop, create the following two variables:
     // One to store the alt attribute of the image at the loop's current iteration, like so: YourImgVariableName[i].alt
     // One for the `event.target.value`
   // 3e) Log out the two variables, refresh the page, and select a new shirt to confirm their values
   // 3f) Still inside the loop, create an if/else statement 
+
+    for (let i = 0; i < shirtImage.length; i++) {
+      const alt = shirtImage[i].alt;
+      const ev = event.target.value;
+
+      console.log(alt);
+      console.log(ev);
+
+      if (alt === ev) {
+        shirtImage[i].classList.add('chosen')
+      } else {
+        shirtImage[i].classList.remove('chosen')
+      }
+    }
+  });
+  
   // 3g) If the two variables you just created are equal, use the `.classList.add()` method to add the 'chosen' className to the img element
   // 3h) Else use the `.classList.remove()` method to remove the 'chosen' className from the from the img element
 
-  console.log("The select element's change event listener is functional!");
-});
